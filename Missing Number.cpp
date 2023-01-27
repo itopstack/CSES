7e8eeae50e87ee16c8b191e3169943eeaba8496c
Kittisak Phetrungnapha <cs.sealsoul@gmail.com>
2023-01-28 01:54:49 +0700

    Solve Missing Number

---
+++ Missing Number.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+
+#define int long long int
+#define double long double
+#define print(a) for (auto x: a) cout << x << " "; cout << endl
+
+using namespace std;
+
+const bool LOOP_TEST_CASES = false;
+
+void solve() {
+	int n, i;
+	cin >> n;
+
+	int sum1 = n * (n + 1) / 2;
+	int sum2 = 0;
+	n--;
+
+	while (n--) {
+		cin >> i;
+		sum2 += i;
+	}
+
+	cout << sum1 - sum2;
+}
+
+int32_t main() {
+	ios_base::sync_with_stdio(0); 
+	cin.tie(0); 
+	cout.tie(0);
+
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	#endif
+	
+	clock_t z = clock();
+
+	int t = 1;
+	if (LOOP_TEST_CASES) {
+		cin >> t;
+	}
+	while (t--) solve();
+
+	cerr << "Run Time : " << ((double)(clock() - z) / CLOCKS_PER_SEC) << endl;
+
+	return 0;
+}
