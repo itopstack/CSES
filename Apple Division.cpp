15bce60c7f509b178c76e7dfaf8ad3a6bc6d8d88
Kittisak Phetrungnapha <cs.sealsoul@gmail.com>
2023-01-28 22:13:02 +0700

    Solve Apple Division

---
+++ Apple Division.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+
+#define int long long int
+#define double long double
+#define print(a) for (auto x: a) cout << x << " "; cout << endl
+
+using namespace std;
+
+const bool LOOP_TEST_CASES = false;
+
+int cal(int a[], int n, int i, int sum1, int sum2) {
+	if (i == n) {
+		return abs(sum1 - sum2);
+	}
+	return min(
+			cal(a, n, i + 1, sum1 + a[i], sum2), cal(a, n, i + 1, sum1, sum2 + a[i])
+		);
+}
+
+void solve() {
+	int n;
+	cin >> n;
+	int a[n];
+
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+
+	cout << cal(a, n, 0, 0, 0);
+}
+
+int32_t main() {
+	ios_base::sync_with_stdio(0); 
+	cin.tie(0); 
+	cout.tie(0);
+
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	#endif
+	
+	clock_t z = clock();
+
+	int t = 1;
+	if (LOOP_TEST_CASES) {
+		cin >> t;
+	}
+	while (t--) solve();
+
+	cerr << "Run Time : " << ((double)(clock() - z) / CLOCKS_PER_SEC) << endl;
+
+	return 0;
+}
