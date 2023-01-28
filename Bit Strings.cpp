94e070eaa8a5c4bca7dbdff210d1d327099b7db5
Kittisak Phetrungnapha <cs.sealsoul@gmail.com>
2023-01-28 14:39:14 +0700

    Solve Bit Strings

---
+++ Bit Strings.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+#define int long long int
+#define double long double
+#define print(a) for (auto x: a) cout << x << " "; cout << endl
+
+using namespace std;
+
+const bool LOOP_TEST_CASES = false;
+
+void solve() {
+	int n;
+	cin >> n;
+	int ans = 1;
+
+	for (int i = 0; i < n; i++) {
+		ans *= 2;
+		ans %= 1000000007;
+	}
+
+	cout << ans;
+}
+
+int32_t main() {
+	ios_base::sync_with_stdio(0); 
+	cin.tie(0); 
+	cout.tie(0);
+
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	#endif
+	
+	clock_t z = clock();
+
+	int t = 1;
+	if (LOOP_TEST_CASES) {
+		cin >> t;
+	}
+	while (t--) solve();
+
+	cerr << "Run Time : " << ((double)(clock() - z) / CLOCKS_PER_SEC) << endl;
+
+	return 0;
+}
