e5a4b4afa349095f99972810d50b85ab73caa686
Kittisak Phetrungnapha <cs.sealsoul@gmail.com>
2023-01-28 21:33:33 +0700

    Solve Creating Strings

---
+++ Creating Strings.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+
+#define int long long int
+#define double long double
+#define print(a) for (auto x: a) cout << x << " "; cout << endl
+
+using namespace std;
+
+const bool LOOP_TEST_CASES = false;
+
+void solve() {
+	string s;
+	cin >> s;
+	sort(s.begin(), s.end());
+	vector<string> v;
+
+	do {
+		v.push_back(s);
+	} while (next_permutation(s.begin(), s.end()));
+
+	cout << v.size() << endl;
+	for (string& i: v) {
+		cout << i << endl;
+	}
+}
+
+int32_t main() {
+	ios_base::sync_with_stdio(0); 
+	cin.tie(0); 
+	cout.tie(0);
+
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	#endif
+	
+	clock_t z = clock();
+
+	int t = 1;
+	if (LOOP_TEST_CASES) {
+		cin >> t;
+	}
+	while (t--) solve();
+
+	cerr << "Run Time : " << ((double)(clock() - z) / CLOCKS_PER_SEC) << endl;
+
+	return 0;
+}
